2838e56e4dd92b806db111701d06b160e600b331
Sohail Shafii <sohail.shafii@gmail.com>
2025-06-19 22:10:18 -0700

    set up test, train etc data sets

--- LogisticRegressionTest/src/main.cpp
+++ LogisticRegressionTest/src/main.cpp
@@ -10,10 +10,16 @@
 char* TryGetArgument(int argIndex, int argc, char **argv);
 void PrintUsage();
 std::vector<std::string> GetFilesInPath(const std::string& path);
+void BuildDatasetInformation(const std::string& positiveLabel,
+                             const std::string& parentFolder,
+                             std::vector<unsigned char*> images,
+                             std::vector<float> yValues);
+void FreeImages(const std::vector<unsigned char*>& trainingSet);
 
 int main(int argc, char** argv)
 {
     std::string trainingPath = "";
+    std::string testPath = "";
     std::string positiveLabel = "";
 	for (int i = 1; i < argc; i++){
         if (!strcmp(argv[i], "-train")) {
@@ -23,6 +29,14 @@ int main(int argc, char** argv)
                 return 1;
             }
             trainingPath = trainingPathVal;
+        }
+        if (!strcmp(argv[i], "-test")){
+            auto testPathVal = TryGetArgument(i + 1, argc, argv);
+            if (testPathVal == nullptr) {
+                std::cerr << "No test path given.\n";
+                return 1;
+            }
+            testPath = testPathVal;
         }
         if (!strcmp(argv[i], "-posLabel")){
             auto positiveLabelVal = TryGetArgument(i + 1, argc, argv);
@@ -34,23 +48,25 @@ int main(int argc, char** argv)
         }
 	}
     
-    if (trainingPath == "" || positiveLabel == "") {
+    if (trainingPath == "" || testPath == "" || positiveLabel == "") {
         PrintUsage();
         return 1;
     }
 
-    std::vector<std::string> trainingPaths = GetFilesInPath(trainingPath);
-    std::cout << "Found " << trainingPaths.size() << " training images.\n";
-    for(auto path : trainingPaths) {
-        int width, height, channels;
-        unsigned char* img = stbi_load(path.c_str(), &width, &height, &channels, 0);
-        if (img == nullptr) {
-            std::cerr << "Could not load image at path " << path << ".\n";
-            return 1;
-        }
-        stbi_image_free(img);
-    }
+    std::vector<unsigned char*> trainingSet;
+    std::vector<float> trainingSetY;
+    BuildDatasetInformation(positiveLabel,
+                            trainingPath,
+                            trainingSet,
+                            trainingSetY);
+    std::vector<unsigned char*> testSet;
+    std::vector<float> testSetY;
+    BuildDatasetInformation(positiveLabel,
+                            testPath,
+                            testSet,
+                            testSetY);
 
+    FreeImages(trainingSet);
 	return 0;
 }
 
@@ -62,9 +78,8 @@ char* TryGetArgument(int argIndex, int argc, char **argv) {
 }
 
 void PrintUsage() {
-    std::cout << "Arguments expected: -train <training_path> -posLabel <positive_label>\n";
+    std::cout << "Arguments expected: -train <training_path> -test <test_path> -posLabel <positive_label>\n";
 }
-
 
 std::vector<std::string> GetFilesInPath(const std::string& path) {
     std::vector<std::string> allPaths;
@@ -73,4 +88,30 @@ std::vector<std::string> GetFilesInPath(const std::string& path) {
         allPaths.push_back(entry.path());
     }
     return allPaths;
+}
+
+void BuildDatasetInformation(const std::string& positiveLabel,
+         const std::string& parentFolder,
+         std::vector<unsigned char*> images,
+         std::vector<float> yValues) {
+    std::vector<std::string> filePaths = GetFilesInPath(parentFolder);
+    std::cout << "Found " << filePaths.size() << " images in path " << parentFolder << ".\n";
+    for(auto path : filePaths) {
+        int width, height, channels;
+        // positive means Y = 1, otherwise it is 0.
+        float yValue = path.find(positiveLabel) != std::string::npos ? 1.0f : 0.0f;
+        unsigned char* img = stbi_load(path.c_str(), &width, &height, &channels, 0);
+        if (img == nullptr) {
+            std::cerr << "Could not load image at path " << path << ".\n";
+            return 1;
+        }
+        images.push_back(img);
+        yValues.push_back(yValue);
+    }
+}
+
+void FreeImages(const std::vector<unsigned char*>& trainingSet) {
+    for(auto img : trainingSet) {
+        stbi_image_free(img);
+    }
 }
