3e4c083548232750486386fbefe58d4ce26eb7a2
Sohail Shafii <sohail.shafii@gmail.com>
2025-06-22 22:20:47 -0700

    add more code like flattening

--- LogisticRegressionTest/src/main.cpp
+++ LogisticRegressionTest/src/main.cpp
@@ -1,11 +1,18 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+
 #include <iostream>
 #include <string>
 #include <cstring>
 #include <vector>
 #include <filesystem>
+#include <cassert>
+
+#define TARGET_IMAGE_SIZE 256
+#define EXPECTED_CHANNELS 3
 
 char* TryGetArgument(int argIndex, int argc, char **argv);
 void PrintUsage();
@@ -14,6 +21,9 @@ void BuildDatasetInformation(const std::string& positiveLabel,
                              const std::string& parentFolder,
                              std::vector<unsigned char*> images,
                              std::vector<float> yValues);
+void ResizeNearest(const unsigned char* src, int oldWidth, int oldHeight, int channels,
+                   unsigned char* dst, int newWidth, int newHeight);
+void FlattenImages(const std::vector<unsigned char*>& trainingSet);
 void FreeImages(const std::vector<unsigned char*>& trainingSet);
 
 int main(int argc, char** argv)
@@ -65,8 +75,23 @@ int main(int argc, char** argv)
                             testPath,
                             testSet,
                             testSetY);
+    
+    size_t mTrain = trainingSet.size();
+    size_t mTest = testSet.size();
+    std::cout << "Number of training examples: " << mTrain
+    << ", number of testing examples: " << mTest
+    << ", width and height of each image: " << TARGET_IMAGE_SIZE << " x " << TARGET_IMAGE_SIZE
+    << ", num channels: " << EXPECTED_CHANNELS << "\n";
+    
+    std::cout << "Flattening images.\n";
+    FlattenImages(trainingSet);
+    FlattenImages(testSet);
+    std::cout << "Done flattening.\n";
 
     FreeImages(trainingSet);
+    trainingSet.clear();
+    FreeImages(testSet);
+    testSet.clear();
 	return 0;
 }
 
@@ -101,17 +126,63 @@ void BuildDatasetInformation(const std::string& positiveLabel,
         // positive means Y = 1, otherwise it is 0.
         float yValue = path.find(positiveLabel) != std::string::npos ? 1.0f : 0.0f;
         unsigned char* img = stbi_load(path.c_str(), &width, &height, &channels, 0);
+        assert(channels == EXPECTED_CHANNELS);
         if (img == nullptr) {
             std::cerr << "Could not load image at path " << path << ".\n";
             return 1;
         }
-        images.push_back(img);
+        unsigned char* output = new unsigned char[TARGET_IMAGE_SIZE*TARGET_IMAGE_SIZE*EXPECTED_CHANNELS];
+        int input_stride = width * channels;
+        int output_stride = TARGET_IMAGE_SIZE * channels;
+        // force a resize
+        ResizeNearest(img, width, height, channels, output, TARGET_IMAGE_SIZE, TARGET_IMAGE_SIZE);
+        images.push_back(output);
         yValues.push_back(yValue);
+        stbi_image_free(img);
+    }
+}
+
+// flatten each image so that the red components are first,
+// followed by green and then blue
+void FlattenImages(const std::vector<unsigned char*>& trainingSet)
+{
+    std::vector<unsigned char> tempImage;
+    int imageSizeSquared = TARGET_IMAGE_SIZE*TARGET_IMAGE_SIZE;
+    int numBytes = imageSizeSquared*EXPECTED_CHANNELS;
+    tempImage.resize(numBytes);
+    for (size_t i = 0; i < trainingSet.size(); i++) {
+        for (int row = 0; row < TARGET_IMAGE_SIZE; row++) {
+            int rowOffsetOriginal = row*TARGET_IMAGE_SIZE*EXPECTED_CHANNELS;
+            int redOffsetDest = row*TARGET_IMAGE_SIZE;
+            int greenOffsetDest = imageSizeSquared + redOffsetDest;
+            int blueOffsetDest = 2 * imageSizeSquared + redOffsetDest;
+            auto image = trainingSet[i];
+            for (int col = 0; col < TARGET_IMAGE_SIZE; col++) {
+                tempImage[redOffsetDest + col] = image[rowOffsetOriginal + col*EXPECTED_CHANNELS];
+                tempImage[greenOffsetDest + col] = image[rowOffsetOriginal + col*EXPECTED_CHANNELS + 1];
+                tempImage[blueOffsetDest + col] = image[rowOffsetOriginal + col*EXPECTED_CHANNELS + 2];
+            }
+        }
+        memcpy(trainingSet[i], tempImage.data(), numBytes);
     }
 }
 
 void FreeImages(const std::vector<unsigned char*>& trainingSet) {
     for(auto img : trainingSet) {
-        stbi_image_free(img);
+        delete [] img;
+    }
+}
+
+void ResizeNearest(const unsigned char* src, int oldWidth, int oldHeight, int channels,
+                   unsigned char* dst, int newWidth, int newHeight) {
+    for (int y = 0; y < newHeight; ++y) {
+        int src_y = y * oldHeight / newHeight;
+        for (int x = 0; x < newWidth; ++x) {
+            int src_x = x * oldWidth / newWidth;
+            for (int c = 0; c < channels; ++c) {
+                dst[(y * newWidth + x) * channels + c] =
+                    src[(src_y * oldWidth + src_x) * channels + c];
+            }
+        }
     }
 }
